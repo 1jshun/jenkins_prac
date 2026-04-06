db426562d37e8d67d95318d383e7b4ffaa718bcc
NahunE <myjsh6547@naver.com>
2026-04-06 10:20:28 +0900

    first commit

---
+++ src/test/java/org/example/jenkins_prac/JenkinsPracApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jenkins_prac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsPracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
